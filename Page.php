2af5e6600b46a26da753edc6dbe8251ff772bf1f
Nima <nima5395@gmail.com>
2019-01-13 13:16:42 +0100

    Kunde now refreshes every 5 seconds

--- Page.php
+++ Page.php
@@ -26,7 +26,13 @@ abstract class Page
 <!DOCTYPE html>
 <html lang="de">
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8">\n
+HTML;
+        if ($headline == "Kunde")
+            echo<<<HTML
+        <meta http-equiv="refresh" content="5">\n
+HTML;
+        echo<<<HTML
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
         <link rel="stylesheet" href="css/master.css"/>
