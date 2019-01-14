ca1de80ac4eba5b0ef1aaf6f466b69adaef2fe81
AlexRothauge <alexrothauge@gmail.com>
2019-01-14 15:28:48 +0100

    Kommas hinzugefügt im Insert Script vür Angebot

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,7 +1,7 @@
 /**
 * PIZZEN
 */
-INSERT INTO ANGEBOT VALUES ("Margherita", 430, "img/margherita.png");
-INSERT INTO ANGEBOT VALUES ("Salami", 480, "img/salami.png");
-INSERT INTO ANGEBOT VALUES ("Peperoni", 480, "img/peperoni.png");
-INSERT INTO ANGEBOT VALUES ("Speziale", 580, "img/speziale.png");
+INSERT INTO ANGEBOT VALUES ("Margherita", 4.30, "img/margherita.png");
+INSERT INTO ANGEBOT VALUES ("Salami", 4.80, "img/salami.png");
+INSERT INTO ANGEBOT VALUES ("Peperoni", 4.80, "img/peperoni.png");
+INSERT INTO ANGEBOT VALUES ("Speziale", 5.80, "img/speziale.png");
